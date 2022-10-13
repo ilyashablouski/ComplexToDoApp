085ebbb5cd36a44f4d1076eb06f0e85e6405b3e8
ilyashablouski <ilya.shablovskiy@gmail.com>
2022-10-13 03:07:21 +0300

    add TextInput component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
 import Link from '@/components/atoms/Link';
-
 import './App.css';
+import TextInput from '@/components/atoms/TextInput';
 
 function App() {
   return (
     <div className="App">
+      <TextInput />
+
       <Link className="App-link" href="https://reactjs.org" isTarget={true}>
         Learn React
       </Link>
-
-      <input type="text" ref="textInput" />
     </div>
   );
 }
---
+++ src/components/atoms/TextInput/TextInput.tsx
@@ -0,0 +1,13 @@
+import React, { LegacyRef, useEffect, useRef } from 'react';
+
+const TextInput = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    inputRef.current && inputRef.current.focus();
+  }, []);
+
+  return <input type="text" defaultValue="Uncontrolled input" ref={inputRef} />;
+};
+
+export default TextInput;
