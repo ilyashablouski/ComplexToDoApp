511afe77d4512b28a454ad2211c50e203249e9a8
ilyashablouski <ilya.shablovskiy@gmail.com>
2022-10-17 02:11:13 +0300

    delete unused import

--- src/components/atoms/TextInput/TextInput.tsx
+++ src/components/atoms/TextInput/TextInput.tsx
@@ -1,4 +1,4 @@
-import React, { LegacyRef, useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 const TextInput = () => {
   const inputRef = useRef<HTMLInputElement>(null);
