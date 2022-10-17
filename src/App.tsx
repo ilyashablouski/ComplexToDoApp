09163453923b2c2baf7a16180f71c868c1b61c03
ilyashablouski <ilya.shablovskiy@gmail.com>
2022-10-18 01:56:32 +0300

    add first structure of to Do list

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,26 @@ import React from 'react';
 import Link from '@/components/atoms/Link';
 import './App.css';
 import TextInput from '@/components/atoms/TextInput';
+import Button from '@/components/atoms/Button';
+import TrashIcon from '@/assets/icons/Trash';
 
 function App() {
   return (
     <div className="App">
-      <TextInput />
+      <div className="form-group">
+        <TextInput />
+
+        <Button children="Add Task" />
+      </div>
+
+      <div className="list-wrapper">
+        <ul className="list">
+          <li className="list-item">
+            <span className="list-item__text">Some text</span>
+            <Button children={<TrashIcon />} />
+          </li>
+        </ul>
+      </div>
 
       <Link className="App-link" href="https://reactjs.org" isTarget={true}>
         Learn React
